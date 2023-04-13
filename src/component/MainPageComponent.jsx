7a65c8d040d751403d6371e093d8451848a92b10
nick <lizheng9024@gmail.com>
2023-04-13 00:16:20 -0400

    push

--- src/component/MainPageComponent.jsx
+++ src/component/MainPageComponent.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useRef } from "react";
 import Row from "react-bootstrap";
 
 export default function MainPageComponent() {
+    const myRef = useRef(null)
+
+    const scrollTo = () => {
+        window.scrollTo(0, (myRef.current.offsetTop + myRef.current.getBoundingClientRect().top + window.screenY))
+    }
+
   return (
     <div className="container-fluid">
         <div className="mt-5">
@@ -12,15 +18,20 @@ export default function MainPageComponent() {
             <div><span className="headline2 fs-1">A</span> <span className="headline3">full stack</span> <span className="headline4">software engineer.</span></div>
         </h1>
         <div className="pt-5 mt-5 mb-5 pb-5">
-            <button className="btn btn-sm bg-dark text-light">Discover</button>
+            <a className="btn btn-sm bg-dark text-light" onClick={scrollTo}>Discover</a>
         </div>
-        <div className="row">
-            <div className="col-sm-6">
-                <div className="col-sm-6 fs-5 w-100">I build fast responsive modern web applications.</div>
-                <div className="row">The tools used on the projects are JAVA, Spring Boot, MySQL, React, JavaScript, BootStrap, HTML5, CSS, Python, TensorFlow, etc.</div>
+        <div ref={myRef} className="row align-items-center mt-5 pt-5">
+            <div className="col-sm-6 mt-5">
+                <div className="fs-4 w-100 fst-italic fw-bold">I build fast responsive modern web applications.</div>
+                <div className="fs-6 mt-1">The tools used on the projects are JAVA, Spring Boot, MySQL, React, JavaScript, BootStrap, HTML5, CSS, Python, TensorFlow, etc.</div>
             </div>
-            <div className="col-sm-6">
-                <img src="/1.png" alt="2" className="w-100" style={{size:'50px'}}/>
+            <div className="col-sm-6 mt-5">
+                <img src="/1.png" alt="2" className="w-75"/>
+            </div>
+        </div>
+        <div>
+            <div>
+                <div>About me</div>
             </div>
         </div>
     </div>
